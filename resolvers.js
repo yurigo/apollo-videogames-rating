410dc99107147219b903fa4a6a051deb099932e5
Yuri <ygonzoo@gmail.com>
2021-04-10 11:17:08 +0200

    Added resolvers to videogame

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,7 @@
 module.exports = {
     Query: {
-        videogames: async (_, __, { dataSources }) => {
-            let data = await dataSources.VideogameDAO.all();
-            data = data.map((e) => {
-                e.eman = e.name.split("").reverse().join("");
-                return e;
-            });
-            return data;
-        },
+        videogames: async (_, __, { dataSources }) =>
+            await dataSources.VideogameDAO.all(),
         videogame: async (_, { id }, { dataSources }) =>
             await dataSources.VideogameDAO.get(id),
         users: async (_, __, { dataSources }) =>
@@ -16,45 +10,19 @@ module.exports = {
             await dataSources.UserDAO.get(id),
     },
 
-    // Mutation: {
-    //     videogame: async (_, args, { dataSources }) => {
-    //         const restaurante = {};
-    //         restaurante.nombre = args.nombre ? args.nombre : undefined;
-    //         restaurante.x = args.x ? args.x : undefined;
-    //         restaurante.y = args.y ? args.y : undefined;
-    //         let response = await dataSources.RestauranteDAO.insert(restaurante);
-    //         restaurante.id = response.insertId;
-    //         return restaurante;
-    //     },
-    //     user: async (_, args, { dataSources }) => {
-    //         const usuario = {};
-    //         usuario.nombre = args.nombre;
-    //         usuario.login = args.login;
-    //         let response = await dataSources.UsuarioDAO.insert(usuario);
-    //         usuario.id = response.insertId;
-    //         return usuario;
-    //     },
-    //     score: async (_, args, { dataSources }) => {
-    //         const puntuacion = {};
-    //         puntuacion.restaurante = args.restaurante;
-    //         puntuacion.usuario = args.usuario;
-    //         puntuacion.valoracion = args.valoracion;
-
-    //         console.log(puntuacion);
-
-    //         let response = await dataSources.PuntuacionDAO.puntua(puntuacion);
-    //         return puntuacion;
-    //     },
-    // },
-
     Videogame: {
-        scores: async ({ id }, _, { dataSources }) =>
+        eman: async ({ name }) => name.split("").reverse().join(""),
+        score: async ({ id }, _, { dataSources }) =>
             await dataSources.ScoreDAO.GetScoreVideogame(id),
+        count: async ({ id }, _, { dataSources }) =>
+            await dataSources.ScoreDAO.GetCountVideogame(id),
+        scores: async ({ id }, _, { dataSources }) =>
+            await dataSources.ScoreDAO.GetScoresVideogame(id),
     },
 
     User: {
         scores: async ({ id }, _, { dataSources }) =>
-            await dataSources.ScoreDAO.GetScoreUser(id),
+            await dataSources.ScoreDAO.GetScoresUser(id),
     },
 
     Score: {
